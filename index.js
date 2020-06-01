63181a210e85c1d05153fc0fedc688155f18e30c
Ivo Mladenov <imladenov1997@gmail.com>
2020-06-01 20:02:16 +0300

    dataset added

--- index.js
+++ index.js
@@ -1,26 +1,166 @@
 const { F1TelemetryClient, constants } = require('f1-telemetry-client');
 const { PACKETS } = constants;
 const axios = require('axios');
+const fs = require('fs');
 
 const client = new F1TelemetryClient();
-// client.on(PACKETS.session, console.log);
-// client.on(PACKETS.motion, console.log);
-// client.on(PACKETS.lapData, console.log);
+client.on(PACKETS.session, getSessionData);
+client.on(PACKETS.motion, getCarMotionData);
+client.on(PACKETS.lapData, getLapData);
 // client.on(PACKETS.event, console.log);
 // client.on(PACKETS.participants, console.log);
-// client.on(PACKETS.carSetups, console.log);
-client.on(PACKETS.carTelemetry, logImportant);
-// client.on(PACKETS.carStatus, console.log);
+client.on(PACKETS.carSetups, getCarSetup);
+client.on(PACKETS.carTelemetry, getCarTelemetryData);
+client.on(PACKETS.carStatus, getCarStatusData);
+
+let sessionData = [];
+let motionData = [];
+let lapData = [];
+let carSetupData = [];
+let telemetryData = [];
+let carStatusData = [];
+
+let sessionHeader = null;
+let motionDataHeader = null;
+let lapDataHeader = null;
+let carSetupDataHeader = null;
+let telemetryDataHeader = null;
+let carStatusDataHeader = null;
 
 // to start listening:
 client.start();
 
 function logImportant(packet) {
     let header = packet.m_header;
-    let individualData = packet.m_carTelemetryData[2];
-    let someone = packet.m_carTelemetryData[3]
+    let individualData = packet.m_carTelemetryData[0];
 
-    compare(header, individualData, someone);
+    addRecord(header, individualData);
+}
+
+function getCarStatusData(packet) {
+    let data = packet.m_carStatusData[19];
+    let basicData = Object.keys(data).filter(key => typeof data[key] !== 'object');
+    let dataPoint = basicData.reduce((accum, item) => {
+        return accum + data[item] + ',';
+    }, '');
+    let tyresWear = data.m_tyresWear.join(',');
+    let tyresDamage = data.m_tyresDamage.join(',');
+
+    dataPoint += [tyresWear, tyresDamage].join(',');
+
+    carStatusData.push(dataPoint);
+
+    if (carStatusDataHeader == null) {
+        basicData.push('tyresWear', 'tyresDamage');
+        carStatusDataHeader = basicData.join(',');
+    }
+
+    console.log(dataPoint);
+
+    return dataPoint
+}
+
+
+function getCarTelemetryData(packet) {
+    let data = packet.m_carTelemetryData[19];
+    let basicData = Object.keys(data).filter(key => typeof data[key] !== 'object');
+    let dataPoint = basicData.reduce((accum, item) => {
+        return accum + data[item] + ',';
+    }, '');
+    let m_brakesTemperature = data.m_brakesTemperature.join(',');
+    let m_tyresSurfaceTemperature = data.m_tyresSurfaceTemperature.join(',');
+    let tyresInnerTemperature = data.m_tyresInnerTemperature.join(',');
+    let tyresPressure = data.m_tyresPressure.join(',');
+    let surfaceType = data.m_surfaceType.join(',');
+    let arrays = [m_brakesTemperature, m_tyresSurfaceTemperature, tyresInnerTemperature, tyresPressure, surfaceType].join(',');
+
+    dataPoint += arrays;
+
+    console.log('TELEMETRY');
+    console.log(dataPoint);
+
+    telemetryData.push(dataPoint);
+
+    if (telemetryDataHeader == null) {
+        basicData.push('m_brakesTemperature', 'm_tyresSurfaceTemperature', 'm_tyresInnerTemperature', 'm_tyresPressure', 'm_surfaceType');
+        telemetryDataHeader = basicData.join(',');
+    }
+
+    return dataPoint;
+}
+
+function getCarSetup(packet) {
+    let keys = Object.keys(packet.m_carSetups[19]);
+    let lastKey = keys.pop()
+    let dataPoint = keys.reduce((accum, item) => {
+        return accum + packet.m_carSetups[19][item] + ',';
+    }, '');
+    dataPoint += packet.m_carSetups[19][lastKey];
+
+    carSetupData.push(dataPoint);
+
+    if (carSetupDataHeader == null) {
+        keys.push('m_brakesTemperature', 'm_tyresSurfaceTemperature', 'm_tyresInnerTemperature', 'm_tyresPressure', 'm_surfaceType');
+        carSetupDataHeader = keys.join(',');
+    }
+
+    console.log('CARSETUP');
+    console.log(dataPoint);
+}
+
+function getLapData(packet) {
+    let currentLap = packet.m_lapData[19];
+    let keys = Object.keys(currentLap);
+    let last = keys.pop();
+    let dataPoint = keys.reduce((accum, item) => {
+        return accum + currentLap[item] + ',';
+    }, '');
+    dataPoint += currentLap[last];
+
+    lapData.push(dataPoint);
+
+    if (lapDataHeader == null) {
+        lapDataHeader = keys.join(',');
+    }
+
+    console.log('LAPDATA');
+    console.log(dataPoint);
+}
+
+function getSessionData(packet) {
+    let keys = Object.keys(packet).filter(key => typeof packet[key] !== 'object');
+    let last = keys.pop();
+    let dataPoint = keys.reduce((accum, item) => {
+        return accum + packet[item] + ',';
+    }, '');
+    dataPoint += packet[last];
+
+    sessionData.push(dataPoint);
+
+    if (sessionHeader == null) {
+        sessionHeader = keys.join(',');
+    }
+
+    console.log('SESSIONDATA');
+    console.log(dataPoint);
+}
+
+function getCarMotionData(packet) {
+    let keys = Object.keys(packet.m_carMotionData[19]);
+    let lastKey = keys.pop()
+    let dataPoint = keys.reduce((accum, item) => {
+        return accum + packet.m_carMotionData[19][item] + ',';
+    }, '');
+    dataPoint += packet.m_carMotionData[19][lastKey];
+
+    motionData.push(dataPoint);
+
+    if (motionDataHeader == null) {
+        motionDataHeader = keys.join(',');
+    }
+
+    console.log('MOTIONDATA');
+    console.log(dataPoint);
 }
 
 function addRecord(header, data_unit) {
@@ -76,3 +216,35 @@ function compare(header, individualData, someonesData) {
         });
     });
 };
+
+process.on('SIGINT', e => {
+
+    console.log(sessionHeader);
+    console.log('==========');
+    console.log(motionDataHeader);
+    console.log('==========');
+    console.log(lapDataHeader);
+    console.log('==========');
+    console.log(carSetupDataHeader);
+    console.log('==========');
+    console.log(telemetryDataHeader);
+    console.log('==========');
+    console.log(carStatusDataHeader);
+
+    client.stop();
+
+    let allData = [
+        [sessionData, 'sessionData.csv', sessionHeader], 
+        [motionData, 'motionData.csv', motionDataHeader], 
+        [lapData, 'lapData.csv', lapDataHeader], 
+        [carSetupData, 'carSetupData.csv', carSetupDataHeader], 
+        [telemetryData, 'telemetryData.csv', telemetryDataHeader], 
+        [carStatusData, 'carStatusData.csv', carStatusDataHeader]
+    ];
+    allData.map(data => {
+        fs.appendFileSync(data[1], data[2] + '\n', console.log);
+        data[0].forEach(line => fs.appendFileSync(data[1], line + '\n', console.log));
+    });
+
+    process.exit();
+});
